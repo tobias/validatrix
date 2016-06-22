66fe941e70eae29ebb8754c4909a89d84aa62746
Toby Crawley <toby@tcrawley.org>
2016-06-22 15:17:45 -0400

    Comment on what each dep is used for

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.apache.ws.xmlschema/xmlschema-walker "2.2.1"]
-                 [clemence "0.2.0"]
-                 [myguidingstar/clansi "1.3.0"]]
+                 [org.apache.ws.xmlschema/xmlschema-walker "2.2.1"] ;; used to turn the schema into a tree we can search
+                 [clemence "0.2.0"]                         ;; provides levenshtein distance calculation to detect misspellings
+                 [myguidingstar/clansi "1.3.0"]]            ;; ansi color codes for colorized output
   :main validatrix.main
   :profiles {:dev {:dependencies [[spyscope "0.1.5"]]}})
 
